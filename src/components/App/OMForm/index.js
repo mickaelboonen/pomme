5e870ed2595c24b9dce8913ba5f14dc16ae62012
mickaelboonen <mickaelboonen@gmail.com>
2022-11-17 11:48:42 +0100

    began to work on form components

--- src/components/App/OMForm/index.js
+++ src/components/App/OMForm/index.js
@@ -14,42 +14,20 @@ import Thread from '../../generics/Thread';
 
 const OMForm = ({ step }) => {  
   return (
-    <div>
+    <div className='form-container'>
       <Thread step={step} />
       <div className="form-page__title">
         <PageTitle>Création d'un Ordre de Mission</PageTitle>
       </div>
-      {step === 1 && (
-        <div className="form-page__container">
-          <Mission step={step} />
-          <button className="form-page__container-link" type='button'>Enregistrer en l'état et revenir plus tard</button>
-        </div>
-        
-      )}
-      {step === 2 && (
-        <div className="form-page__container">
-          <Transports step={step} />
-          <button className="form-page__container-link" type='button'>Enregistrer en l'état et revenir plus tard</button>
-        </div>
-      )}
-      {step === 3 && (
-        <div className="form-page__container">
-          <Hebergement step={step} />
-          <button className="form-page__container-link" type='button'>Enregistrer en l'état et revenir plus tard</button>
-        </div>
-      )}
-      {step === 4 && (
-        <div className="form-page__container">
-          <Avance step={step} />
-          <button className="form-page__container-link" type='button'>Enregistrer en l'état et revenir plus tard</button>
-        </div>
-      )}
-      {step === 5 && (
-        <div className="form-page__container">
-          <Signature step={step} />
-          <button className="form-page__container-link" type='button'>Retour : Avance</button>
-        </div>
-      )}
+      <div className="form-page__container">
+        {step === 1 && <Mission step={step} />}
+        {step === 2 && <Transports step={step} />}
+        {step === 3 && <Hebergement step={step} />}
+        {step === 4 && <Avance step={step} />}
+        {step === 5 && <Signature step={step} />}
+        {step !== 5 && <button className="form-page__container-link" type='button'>Enregistrer en l'état et revenir plus tard</button>}
+        {step === 5 && <button className="form-page__container-link" type='button'>Retour : Avance</button>}
+      </div>
     </div>
   );
 };
