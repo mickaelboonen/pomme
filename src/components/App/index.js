718610de9a3903b1a2eb6cdc412f952027be6f92
mickaelboonen <mickaelboonen@gmail.com>
2022-11-21 11:07:18 +0100

    created add-vehicle form and page

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -10,6 +10,7 @@ import Gestionnaires from './Gestionnaire';
 import DocValidationForm from './Gestionnaire/DocValidationForm';
 import DocRefusalForm from './Gestionnaire/DocRefusal';
 import MyAccount from './MyAccount';
+import AddVehicle from './MyAccount/AddVehicle';
 
 // == Composant
 function App() {
@@ -31,12 +32,15 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/utilisateur/:slug/mes-ordres-de-mission" element={<MyDocuments />} />
         <Route path="/utilisateur/:slug/mes-états-de-frais" element={<MyDocuments />} />
-        <Route path="/utilisateur/:slug/mes-%C3%A9tats-de-frais" element={<MyDocuments />} />
         <Route path="/gestionnaire/:slug/documents-a-signer" element={<Gestionnaires />} />
         <Route path="/gestionnaire/:slug/valider-un-document/:slug/:id" element={<DocValidationForm />} />
         <Route path="/gestionnaire/:slug/refuser-un-ordre-de-mission/:id" element={<DocRefusalForm />} />
         <Route path="/documents/:slug/nouveau" element={<Forms />} />
         <Route path="/utilisateur/:slug/mes-documents" element={<MyAccount />} />
+        <Route path="/utilisateur/:slug/mes-documents/ajouter-un-vehicule" element={<AddVehicle />} />
+        {/* TODO */}
+        <Route path="/utilisateur/:slug/mes-documents/ajouter-un-v%C3%A9hicule" element={<AddVehicle />} />
+        <Route path="/utilisateur/:slug/mes-%C3%A9tats-de-frais" element={<MyDocuments />} />
       </Routes>
       <script type="text/javascript" src="bootstrap-datetimepicker.de.js" charSet="UTF-8"></script>
     </div>
