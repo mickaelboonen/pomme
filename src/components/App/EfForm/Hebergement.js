5efa32ec285b1004ba2fde5f26551a80d5d54428
mickaelboonen <mickaelboonen@gmail.com>
2022-11-24 11:06:43 +0100

    EF hotel and meals ok

--- src/components/App/EfForm/Hebergement.js
+++ src/components/App/EfForm/Hebergement.js
@@ -9,6 +9,7 @@ import Buttons from './Fields/Buttons';
 import RadioInput from './Fields/RadioInput';
 import SwitchButton from '../../generics/SwitchButton';
 import TextField from './Fields/TextField';
+import FileField from './Fields/FileField';
 
 const Hebergement = ({ step }) => {
   const navigate = useNavigate();
@@ -21,82 +22,92 @@ const Hebergement = ({ step }) => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
-
-    // TODO : Process Data
-
-    // Next Step
-    const nextStep = step++;
-    navigate('/documents/ordre-de-mission/nouveau?etape=' + step++);
-
-    
+    console.log(data);    
   };
 
   let refusal = "Vous avez fait des erreurs au niveau de l'hébergement et des transports. Merci de corriger.";
   refusal = "";
 
-const handleHotelSwitch = (event) => {
-  const { checked } = event.target;
-    const section = event.target.closest('.form__section-field');
-    const nightNumberField = section.nextSibling;
-    const nightPaymentField = nightNumberField.nextSibling;
+  <TextField
+  id="outside-meals-number"
+  formField="outside-meals-number"
+  register={register}
+  isNumber
+  min="0"
+  label="Nombre de repas payés par l'agent"
+/>
+  const mealFields = [
+    {
+      id: 'admin-restaurant-field',
+      formField: 'admin-restaurant',
+      label: 'Repas pris dans un restaurant administratif ou assimilé',
+    },
+    {
+      id: 'paid-by-agent-in-France-field',
+      formField: 'paid-by-agent-in-France',
+      label: 'Repas à titre onéreux en France',
+    },
+    {
+      id: 'free-in-France-field',
+      formField: 'free-in-France',
+      label: 'Repas à titre gratuit en France',
+    },
+    {
+      id: 'paid-by-agent-overseas-field',
+      formField: 'paid-by-agent-overseas',
+      label: "Repas à titre onéreux à l'étranger",
+    },
+  ];
 
-  if (checked) {
-    nightNumberField.classList.remove('form__section-field--hidden');
-    nightPaymentField.classList.remove('form__section-field--hidden');
-  }
-  else {
-    nightNumberField.classList.add('form__section-field--hidden');
-    nightPaymentField.classList.add('form__section-field--hidden');
-  }
-
-};
   return (
     <form className="form" onSubmit={handleSubmit(onSubmit)}>
-      <div className="form__section">
-        <FormSectionTitle>Hébergement</FormSectionTitle>
-        <div className="form__section-field">
-          <SwitchButton
+      <FormSectionTitle>Hébergement</FormSectionTitle>
+      <div className='form__section form__section--documents' id="other-fields">
+        <div className='form__section-half'>
+          <TextField
+            isNumber
+            min='0'
+            id="hotel-field"
+            formField="hotel"
             register={register}
-            handler={handleHotelSwitch}
-            isInForm
-            formField={'hotel'}
-            label="Hotel :"
+            label="Hébergement à titre onéreux (France et étranger)"
+            placeholder="Montant"
           />
         </div>
-        <TextField
-          id="nights-number"
-          formField="nights-number"
-          register={register}
-          min="0"
-          isNumber
-          isHidden
-          label="Nombre de nuits"
-        />
-        <div className="form__section-field form__section-field--hidden">
-          <p className="form__section-field-label">Réglement</p>
-          <RadioInput id="unimes" formField="hotel-payment" label="Payé par Unîmes" register={register} />
-          <RadioInput id="agent" formField="hotel-payment" label="Avancé par l'agent" register={register} />
+        <div className='form__section-half'>
+          <FileField
+            register={register}
+            formField="hotel-files"
+            id="hotel-files-field"
+            multiple
+            label="Facture"
+            placeholder=""
+          />
         </div>
       </div>
-      <div className="form__section">
+      <TextField 
+        isNumber
+        min='0'
+        id="free-accomodation-field"
+        formField="free-accomodation"
+        register={register}
+        label="Hébergement à titre gratuit"
+        placeholder="Nombre de nuits"
+      />
+      <div className="form__section-field" id="meals">
         <FormSectionTitle>Repas</FormSectionTitle>
-        <TextField
-          id="outside-meals-number"
-          formField="outside-meals-number"
-          register={register}
-          isNumber
-          min="0"
-          label="Nombre de repas payés par l'agent"
-        />
-        <TextField
-          id="admin-meals-number"
-          formField="admin-meals-number"
-          register={register}
-          isNumber
-          min="0"
-          label="Nombre de repas en restaurant administratif"
-        />
+        {mealFields.map((field) => (
+          <TextField
+            key={field.id}
+            id={field.id}
+            formField={field.formField}
+            register={register}
+            isNumber
+            min="0"
+            placeholder="Nombre de repas"
+            label={field.label}
+          />
+        ))}
       </div>
       {refusal !== '' && <RefusalMessage message={refusal} />}
       <Buttons step={step} />
