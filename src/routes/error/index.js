f58f74e88d72a79773c36e71f3babaa9356be053
mickaelboonen <mickaelboonen@gmail.com>
2022-11-25 16:49:08 +0100

    added header to 404 page

--- src/routes/error/index.js
+++ src/routes/error/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Header from 'src/routes/layout/Header';
+
 import './style.scss';
 
 const Error = () => (
-  <div>Error</div>
+  <div>
+    <Header />
+    Error
+  </div>
 );
 
 Error.propTypes = {
