35cee8ed5b1745ee148ded1d19a7c8435c333e51
mickaelboonen <mickaelboonen@gmail.com>
2022-12-08 14:22:00 +0100

    om Fom Hebergement ok for now

--- src/routes/documents/OMForm/Hebergement.js
+++ src/routes/documents/OMForm/Hebergement.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
 
@@ -9,11 +9,15 @@ import Buttons from 'src/components/Fields/Buttons';
 import RadioInput from 'src/components/Fields/RadioInput';
 import SwitchButton from 'src/components/SwitchButton';
 import TextField from 'src/components/Fields/TextField';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Hebergement = ({ step }) => {
+    console.log('rendu');
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const {
     register,
+    unregister,
     handleSubmit,
     watch,
     formState:
@@ -21,19 +25,94 @@ const Hebergement = ({ step }) => {
   } = useForm();
 
   const onSubmit = (data) => {
-    console.log(data);
-
+    console.log("------------------------------------------------------", data, "------------------------------------------------------");
+    // console.log(maxMealsNumber);
+    const mealsErrorElement = document.getElementById('meals-error');
+    const totalMeals = Number(data.adminMealsNumber) + Number(data.outsideMealsNumber);
+    
+    if (totalMeals > data.maxMealsNumber) {
+      mealsErrorElement.textContent = "Vous avez saisi plus de repas qu'autorisé. Vous avez le droit à : " + data.maxMealsNumber+ ' repas dans le cadre de votre mission.';
+      mealsErrorElement.classList.add('form__section-field-error--open')
+    }
+    else {
+      
+      mealsErrorElement.textContent = "";
+      mealsErrorElement.classList.remove('form__section-field-error--open')
+      delete data.maxMealsNumber;
+      localStorage.setItem('hebergement', JSON.stringify(data))
+      
+    }
     // TODO : Process Data
-
-    // Next Step
-    const nextStep = step++;
-    navigate('/nouveau-document/ordre-de-mission?etape=' + step++);
+    
 
     
   };
 
   let refusal = "Vous avez fait des erreurs au niveau de l'hébergement et des transports. Merci de corriger.";
   refusal = "";
+
+  const mission = JSON.parse(localStorage.getItem('mission'));
+
+  const handleMealsDependingOnHour = (hour, mealNumber) => {
+
+    if (hour > 21) {
+
+    }
+    else if (hour > 14) {
+      mealNumber += 1;
+    }
+    else {
+      mealNumber += 2;
+
+    }
+    return mealNumber;
+  };
+
+  const getMaxMeals = (mission) => {
+    let maxMealNumber = 0;
+    const depart = new Date(mission.departure);
+    const back = new Date(mission.return);
+    
+    const firstDay = depart.getDate();
+    const lastDay = back.getDate();
+    const timeToDepart = depart.getHours();
+    const timeToLeave = back.getHours();
+    const fullDays = lastDay - firstDay - 1;
+    maxMealNumber += (fullDays * 2);
+
+    maxMealNumber = handleMealsDependingOnHour(timeToDepart, maxMealNumber);
+    maxMealNumber = handleMealsDependingOnHour(timeToLeave, maxMealNumber);
+
+    return maxMealNumber;
+  }
+
+  const getMaxNights = (mission) => {
+    const depart = new Date(mission.departure);
+    const back = new Date(mission.return);
+    
+    const firstDay = depart.getDate();
+    const lastDay = back.getDate();
+    return lastDay - firstDay;
+  }
+
+  const maxMealNumber = getMaxMeals(mission);
+  const maxNightsNumber = getMaxNights(mission);
+
+
+
+  const hotel = watch("hotel");
+
+  useEffect(() => {
+    if (hotel) {
+      register("hotelPayment", {
+        required: "Merci de renseigner le champ."
+      });
+      register("nightsNumber", {
+        required: "Merci de renseigner le nombre de nuits d'hôtel."
+      });
+
+    }
+  })
 
 const handleHotelSwitch = (event) => {
   const { checked } = event.target;
@@ -60,44 +139,58 @@ const handleHotelSwitch = (event) => {
             register={register}
             handler={handleHotelSwitch}
             isInForm
-            formField={'hotel'}
+            formField="hotel"
             label="Hotel :"
           />
         </div>
         <TextField
-          id="nights-number"
-          formField="nights-number"
+          id="nights-number-field"
+          formField="nightsNumber"
           register={register}
           min="0"
+          max={maxNightsNumber}
           isNumber
           isHidden
+          error={errors.nightsNumber}
           label="Nombre de nuits"
+          placeholder="Vous ne pouvez saisir plus de nuits que le nombre calculé selon vos dates de missions."
         />
         <div className="form__section-field form__section-field--hidden">
           <p className="form__section-field-label">Réglement</p>
-          <RadioInput id="unimes" formField="hotel-payment" label="Payé par Unîmes" register={register} />
-          <RadioInput id="agent" formField="hotel-payment" label="Avancé par l'agent" register={register} />
+          <RadioInput id="unimes" formField="hotelPayment" label="Payé par Unîmes" register={register} />
+          <RadioInput id="agent" formField="hotelPayment" label="Avancé par l'agent" register={register} />
+          {errors.hotelPayment && <p className='form__section-field-error form__section-field-error--open'>{errors.hotelPayment.message}</p>}
         </div>
       </div>
       <div className="form__section">
         <FormSectionTitle>Repas</FormSectionTitle>
         <TextField
-          id="outside-meals-number"
-          formField="outside-meals-number"
+          id="outside-meals-number-field"
+          formField="outsideMealsNumber"
           register={register}
           isNumber
           min="0"
+          max={maxMealNumber}
           label="Nombre de repas payés par l'agent"
+          required="Merci de renseigner le nombre de repas prévus à la charge de l'agent"
+          placeholder="Vous ne pouvez saisir plus de repas que le nombre calculé selon vos dates de missions."
+          error={errors.outsideMealsNumber}
         />
         <TextField
-          id="admin-meals-number"
-          formField="admin-meals-number"
+          id="admin-meals-number-field"
+          formField="adminMealsNumber"
           register={register}
           isNumber
           min="0"
+          max={maxMealNumber}
           label="Nombre de repas en restaurant administratif"
+          required="Merci de renseigner le nombre de repas en restaurant administratif"
+          placeholder="Vous ne pouvez saisir plus de repas que le nombre calculé selon vos dates de missions."
+          error={errors.adminMealsNumber}
         />
+        <input id="max-meals-field" name="max-meals-field" type="hidden" value={maxMealNumber} {...register('maxMealsNumber')} />
       </div>
+      <p id="meals-error" className="form__section-field-error" />
       {refusal !== '' && <RefusalMessage message={refusal} />}
       <Buttons step={step} />
     </form>
