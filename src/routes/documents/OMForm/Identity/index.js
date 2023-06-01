cc8d35ccbca542200d24845e431aa3a5df803834
mickaelboonen <mickaelboonen@gmail.com>
2023-06-01 17:06:33 +0200

    added button to visualize PDF directly in the browser

--- src/routes/documents/OMForm/Identity/index.js
+++ src/routes/documents/OMForm/Identity/index.js
@@ -63,165 +63,188 @@ const Identity = ({ isEfForm }) => {
     dispatch(uploadFile({ data: {docId: omId , file: file}, step: 'om'}))
   }
   
+  const [isPdfVisible, setIsPdfVisible] = useState(false)
+
+  const toggleViewer = (event) => {
+
+    if (event.target.id.includes('closer')) {
+      setIsPdfVisible(false);
+    }
+    else {
+      setIsPdfVisible(true);
+    }
+  }
+  
   return (
-    <form className="form">
-      <div style={{width:"100%", height:"100vh"}}>
-        <PDFViewer>
-          <OmPdf countries={countries} agentSignature={agentSignature} data={currentOM} agent={agentFullData} vehicleTypes={vehicleTypes} />
-        </PDFViewer>
-      </div> 
-      <div className="form__section">
-        <FormSectionTitle>Missionnaire</FormSectionTitle>
-        <div className="form__section form__section--split">
-          <p className="form__section-field-label">Qualité : </p>
-          <RadioInput disabled id="Mme" formField="gender" label="Madame" register={register} />
-          <RadioInput disabled id="M." formField="gender" label="Monsieur" register={register} />
-        </div>
-        <div className='form__section form__section--documents' id="other-fields">
-          <div className='form__section-half'>
-            <TextField
-              id="firstname"
-              disabled
-              formField="firstname"
-              label="Prénom"
-              register={register}
-            /> 
+    <>
+      <form className="form">
+        <div className="form__section">
+          <FormSectionTitle>Missionnaire</FormSectionTitle>
+          <div className="form__section form__section--split">
+            <p className="form__section-field-label">Qualité : </p>
+            <RadioInput disabled id="Mme" formField="gender" label="Madame" register={register} />
+            <RadioInput disabled id="M." formField="gender" label="Monsieur" register={register} />
           </div>
-          <div className='form__section-half'>
-            <TextField
-              id="lastname"
-              disabled
-              formField="lastname"
-              label="Nom de famille"
-              register={register}
-            />
-          </div>
-        </div>
-      </div>
-      <div className="form__section">
-        <FormSectionTitle>Adresses du Missionnaire</FormSectionTitle>
-        <Address
-          register={register}
-          disabled
-          countries={countries}
-          title="Adresse familiale"
-          stepNumber=''
-        />
-        <Address
-          title="Adresse administrative"
-          register={register}
-          disabled
-          countries={countries}
-          stepNumber='Pro'
-        />
-      </div>
-      <div className="form__section">
-        <FormSectionTitle>Personnel</FormSectionTitle>
-        <div className="form__section form__section--split" style={{alignItems: 'center'}}>
-          <div className="form__section__half">
-            <div className="form__section-field">
-              <p className="form__section-field-label">Personnel</p>
-                <RadioInput
-                  register={register}
-                  disabled
-                  formField="employer"
-                  id="unimes"
-                  label="Unîmes"
-                  handler={toggleWorker}
-                />
-                <RadioInput
-                  register={register}
-                  disabled
-                  formField="employer"
-                  id="external"
-                  label="Extérieur"
-                  handler={toggleWorker}
-                />
-            </div>
-          </div>
-          {employer === 'unimes' && (
-            <div className="form__section__half" style={{width: '70%'}}>
-              
+          <div className='form__section form__section--documents' id="other-fields">
+            <div className='form__section-half'>
               <TextField
-                id="category"
+                id="firstname"
                 disabled
-                formField="unimesCategory"
-                label="Catégorie"
-                register={register}
-              /> 
-              <TextField
-                id="status"
-                disabled
-                formField="unimesStatus"
-                label="Statut"
-                register={register}
-              /> 
-              <TextField
-                id="department"
-                disabled
-                formField="unimesDepartment"
-                label="Service / département"
+                formField="firstname"
+                label="Prénom"
                 register={register}
               /> 
             </div>
-          )}
-          {employer === 'external' && (
-            <div className="form__section__half" style={{width: '50%'}}>
+            <div className='form__section-half'>
+              <TextField
+                id="lastname"
+                disabled
+                formField="lastname"
+                label="Nom de famille"
+                register={register}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="form__section">
+          <FormSectionTitle>Adresses du Missionnaire</FormSectionTitle>
+          <Address
+            register={register}
+            disabled
+            countries={countries}
+            title="Adresse familiale"
+            stepNumber=''
+          />
+          <Address
+            title="Adresse administrative"
+            register={register}
+            disabled
+            countries={countries}
+            stepNumber='Pro'
+          />
+        </div>
+        <div className="form__section">
+          <FormSectionTitle>Personnel</FormSectionTitle>
+          <div className="form__section form__section--split" style={{alignItems: 'center'}}>
+            <div className="form__section__half">
               <div className="form__section-field">
-                <p className="form__section-field-label">Fonctionnaire</p>
+                <p className="form__section-field-label">Personnel</p>
                   <RadioInput
                     register={register}
-                    formField="civilWorker"
-                    id="isCivil"
-                    label="Oui"
-                    required={errorMessages.civilWorker}
-                    handler={toggleIsCivil}
+                    disabled
+                    formField="employer"
+                    id="unimes"
+                    label="Unîmes"
+                    handler={toggleWorker}
                   />
                   <RadioInput
                     register={register}
-                    formField="civilWorker"
-                    id="isNotCivil"
-                    label="Non"
-                    required={errorMessages.civilWorker}
-                    handler={toggleIsCivil}
+                    disabled
+                    formField="employer"
+                    id="external"
+                    label="Extérieur"
+                    handler={toggleWorker}
                   />
               </div>
-              {isCivil === 'isNotCivil' && (
-                <TextField
-                  id="other"
-                  formField="externalWorker"
-                  label="Autre (à préciser)"
-                  register={register}
-                  error={errors.externalWorker}
-                  required={errorMessages.externalWorker}
-                /> 
-              )}
             </div>
-          )}
+            {employer === 'unimes' && (
+              <div className="form__section__half" style={{width: '70%'}}>
+                
+                <TextField
+                  id="category"
+                  disabled
+                  formField="unimesCategory"
+                  label="Catégorie"
+                  register={register}
+                /> 
+                <TextField
+                  id="status"
+                  disabled
+                  formField="unimesStatus"
+                  label="Statut"
+                  register={register}
+                /> 
+                <TextField
+                  id="department"
+                  disabled
+                  formField="unimesDepartment"
+                  label="Service / département"
+                  register={register}
+                /> 
+              </div>
+            )}
+            {employer === 'external' && (
+              <div className="form__section__half" style={{width: '50%'}}>
+                <div className="form__section-field">
+                  <p className="form__section-field-label">Fonctionnaire</p>
+                    <RadioInput
+                      register={register}
+                      formField="civilWorker"
+                      id="isCivil"
+                      label="Oui"
+                      required={errorMessages.civilWorker}
+                      handler={toggleIsCivil}
+                    />
+                    <RadioInput
+                      register={register}
+                      formField="civilWorker"
+                      id="isNotCivil"
+                      label="Non"
+                      required={errorMessages.civilWorker}
+                      handler={toggleIsCivil}
+                    />
+                </div>
+                {isCivil === 'isNotCivil' && (
+                  <TextField
+                    id="other"
+                    formField="externalWorker"
+                    label="Autre (à préciser)"
+                    register={register}
+                    error={errors.externalWorker}
+                    required={errorMessages.externalWorker}
+                  /> 
+                )}
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-      <div className="form__section">
-        <div className="form__section-field-buttons" style={{display: 'flex', justifyContent: 'center'}}>
-          <BlobProvider document={<OmPdf countries={countries} agentSignature={agentSignature} data={currentOM} agent={agentFullData} vehicleTypes={vehicleTypes} />}>
-            {({ blob }) => {
+        <div className="form__section">
+          <div className="form__section-field-buttons" style={{display: 'flex', justifyContent: 'center'}}>
+            <BlobProvider document={<OmPdf countries={countries} agentSignature={agentSignature} data={currentOM} agent={agentFullData} vehicleTypes={vehicleTypes} />}>
+              {({ blob }) => {
 
-              const file = new File([blob], currentOM.name, {type: 'pdf'});
-              const fileUrl = URL.createObjectURL(file);
-              
-              setValue('om', file);
+                const file = new File([blob], currentOM.name, {type: 'pdf'});
+                const fileUrl = URL.createObjectURL(file);
+                
+                setValue('om', file);
 
-              return (
-                <a href={fileUrl} download={currentOM.name + '.pdf'} style={{textAlign: 'center'}}>
-                  <button type='button' files={file} onClick={generatePDF}>Valider les données et télécharger l'Ordre de Mission</button>
-                </a>
-              );
-            }}
-          </BlobProvider>
+                return (
+                  <>
+                    <a href={fileUrl} download={currentOM.name + '.pdf'} style={{textAlign: 'center'}}>
+                      <button type='button' files={file} onClick={generatePDF}>Valider les données et télécharger l'Ordre de Mission</button>
+                    </a>
+                    <button type="button" id="viewer-opener" onClick={toggleViewer} style={{marginLeft: '1rem'}}>
+                      Visualiser le document
+                    </button>
+                  </>
+                );
+              }}
+            </BlobProvider>
+          </div>
+          <Link to={"/utilisateur/" + user + "/mes-ordres-de-mission"} style={{display: 'block', marginBottom: '2rem', textAlign: 'center'}}>Retour au menu des Ordres de Mission</Link>
         </div>
-        <Link to={"/utilisateur/" + user + "/mes-ordres-de-mission"} style={{display: 'block', marginBottom: '2rem', textAlign: 'center'}}>Retour au menu des Ordres de Mission</Link>
-      </div>
-    </form>
-    
+      </form>
+      {isPdfVisible && (
+        <div className="pdf-viewer">
+          <div className="pdf-viewer__nav">
+            <p className="pdf-viewer__nav-close" id="viewer-closer" onClick={toggleViewer}>Fermer la fenêtre</p>
+          </div>
+          <PDFViewer>
+            <OmPdf countries={countries} agentSignature={agentSignature} data={currentOM} agent={agentFullData} vehicleTypes={vehicleTypes} />
+          </PDFViewer>
+        </div>
+      )}
+    </>
   );
 };
 
